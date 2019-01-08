d4f9d85695171418a4bd58ea909172c6a828d19d
henrikmathiesen <henrik_mathiesen@hotmail.com>
2019-01-08 20:15:09 +0100

    -basics

--- src/app/reactive-programming-basics/reactive-programming-basics.component.ts
+++ src/app/reactive-programming-basics/reactive-programming-basics.component.ts
@@ -1,8 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-    templateUrl: './reactive-programming-basics.component.html'
-  })
-  export class ReactiveProgrammingBasicsComponent {
-    
+  templateUrl: './reactive-programming-basics.component.html'
+})
+export class ReactiveProgrammingBasicsComponent implements OnInit {
+
+  ngOnInit() {
+
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/resolve
+    // https://github.com/domenic/promises-unwrapping/blob/master/docs/states-and-fates.md
+
+    // function call
+    const func = () => console.log('func');
+    func();
+
+    // Promise
+    const prom = Promise.resolve('promises are promises');
+    prom.then(v => console.log(v));
+
+    // Click event
+    const selector = document.querySelector('.js-click-me');
+    const handler = () => console.log('clicked');
+    selector.addEventListener('click', handler);
   }
+
+}
