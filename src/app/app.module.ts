8045331eda20bded98bba58189f61242bfbefe68
henrikmathiesen <henrik_mathiesen@hotmail.com>
2019-01-27 08:25:59 +0100

    -typeahead search

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -29,8 +30,9 @@ import { TypeaheadSearchInObjectsComponent } from './other';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    NgbModule,
     AppRoutingModule,
-    NgbModule
 
   ],
   bootstrap: [AppComponent]
--- src/app/other/typeahead-search-in-objects/typeahead-search-in-objects.component.ts
+++ src/app/other/typeahead-search-in-objects/typeahead-search-in-objects.component.ts
@@ -1,13 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
+
 import { AdressesService } from 'app/services';
+import { AdressModel } from 'app/models';
 
 @Component({
   templateUrl: './typeahead-search-in-objects.component.html'
 })
-export class TypeaheadSearchInObjectsComponent {
+export class TypeaheadSearchInObjectsComponent implements OnInit {
+
+  model: string;
+  adresses: AdressModel[] = [];
+
 
   constructor(
     private adressesService: AdressesService
-  ) { }
+  ) {
+    this.search = this.search.bind(this);
+  }
 
+  ngOnInit() {
+    this.adresses = this.adressesService.getAdresses();
+  }
+
+  search(text$: Observable<string>) {
+    return text$.pipe(
+      debounceTime(200),
+      distinctUntilChanged(),
+      map((term: string) => {
+        if (term.length < 2) {
+          return [];
+        }
+
+        const matches = this.adresses.filter((adress) => {
+          const cityMatch = adress.city.toLowerCase().indexOf(term.toLowerCase()) > -1;
+          const streetMatch = adress.street.toLowerCase().indexOf(term.toLowerCase()) > -1;
+
+          if (cityMatch || streetMatch) {
+            return adress;
+          }
+        });
+
+        return matches.slice(0, 10);
+      })
+    );
+  }
 }
