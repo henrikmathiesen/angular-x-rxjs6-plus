fd27be11e3c561adf4b3053a17914d76d973efcf
henrikmathiesen <henrik_mathiesen@hotmail.com>
2020-04-04 16:08:10 +0200

    -index into array with enum

--- src/app/other/index-into-array-object/index-into-array-object.component.ts
+++ src/app/other/index-into-array-object/index-into-array-object.component.ts
@@ -3,15 +3,19 @@ import { Component } from '@angular/core';
 import { ButtonsConstant } from './buttons.constant';
 import { ButtonsStateConstant } from './buttons-state.constant';
 
+import { ButtonsEnum } from './buttons.enum';
+import { ButtonsStateEnum } from './buttons-state.enum';
+
 @Component({
     templateUrl: './index-into-array-object.component.html'
 })
 export class IndexIntoArrayObjectComponent {
 
     buttonsConstant = ButtonsConstant;
-    buttonsStateConstant = ButtonsStateConstant;
-
     btnObjState = {};
+
+    buttonsEnum = ButtonsEnum;
+    btnArrayState = [];
 
     btnObjAction(btn: string) {
         for (const property in this.btnObjState) {
@@ -29,15 +33,33 @@ export class IndexIntoArrayObjectComponent {
         return this.btnObjState[btn] === ButtonsStateConstant.active ? 'btn-primary' : 'btn-secondary';
     }
 
+    btnArrayAction(btn: ButtonsEnum) {
+        for (let i = 0; i < this.btnArrayState.length; i++) {
+            this.btnArrayState[i] = ButtonsStateEnum.inactive;
+        }
+
+        this.btnArrayState[btn] = ButtonsStateEnum.active;
+
+        console.log('--- ARRAY ---');
+        console.log(this.btnArrayState);
+        console.log('--- /ARRAY ---');
+    }
+
+    getBtnArrayCssClass(btn: ButtonsEnum) {
+        return this.btnArrayState[btn] === ButtonsStateEnum.active ? 'btn-primary' : 'btn-secondary';
+    }
 }
 
 /* 
 
     OBJ
         - can not type btn as ButtonsConstant, not good
-        - when using object typing, intellisence gets cluttred
+        - when using object typing, intellisence gets cluttered
 
     ARRAY
-        -
+        - can use type btn as enum, good, a number is sent in as the argument
+        - when using enum typing, intellisence is better
+
+    I prefer array with enum
 
 */
