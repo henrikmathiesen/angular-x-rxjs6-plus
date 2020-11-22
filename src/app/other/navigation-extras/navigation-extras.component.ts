d7d78b5d905a730dc10afa6032b559615696d67f
henrikmathiesen <henrik_mathiesen@hotmail.com>
2020-11-22 12:36:15 +0100

    -work in progress

--- src/app/other/navigation-extras/navigation-extras.component.ts
+++ src/app/other/navigation-extras/navigation-extras.component.ts
@@ -1,8 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     templateUrl: './navigation-extras.component.html'
 })
-export class NavigationExtrasComponent {
+export class NavigationExtrasComponent implements OnInit {
+
+    // https://medium.com/coding-in-depth/best-use-of-navigationextras-in-angular-7-plus-d5fb436e298e
+
+    constructor(
+        private router: Router
+    ) { }
+
+    model: any;
+
+    ngOnInit() { 
+        this.model = { name: 'B', age: 20 };
+    }
+
+    goTo() {
+        console.log('ok');
+        // this.router.navigate(['../navigation-extras-destination'], { state });
+
+        // this.router.navigate(['/productdetails'], { 
+        //     state: { productdetails: objToSend }
+        //   });
+    }
 
 }
