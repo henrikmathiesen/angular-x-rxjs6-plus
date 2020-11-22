fa60231a90cd8de3c2ab9c6e925f5eb66dcffeca
henrikmathiesen <henrik_mathiesen@hotmail.com>
2020-11-22 13:04:18 +0100

    -passing state with routing

--- src/app/other/navigation-extras-destination/navigation-extras-destination.component.ts
+++ src/app/other/navigation-extras-destination/navigation-extras-destination.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     templateUrl: './navigation-extras-destination.component.html'
 })
 export class NavigationExtrasDestinationComponent {
+
+    constructor(
+        private router: Router
+    ) {
+        this.model = this.router.getCurrentNavigation().extras && this.router.getCurrentNavigation().extras.state ? this.router.getCurrentNavigation().extras.state : null;
+        console.log(this.model);
+    }
+
+    model: any;
 
 }
--- src/app/other/navigation-extras/navigation-extras.component.ts
+++ src/app/other/navigation-extras/navigation-extras.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
     templateUrl: './navigation-extras.component.html'
@@ -9,17 +9,18 @@ export class NavigationExtrasComponent implements OnInit {
     // https://medium.com/coding-in-depth/best-use-of-navigationextras-in-angular-7-plus-d5fb436e298e
 
     constructor(
-        private router: Router
+        private router: Router,
+        private activatedRoute: ActivatedRoute
     ) { }
 
     model: any;
 
     ngOnInit() { 
-        this.model = { name: 'B', age: 20 };
+        this.model = { name: 'From Model', age: 20 };
     }
 
     goTo() {
-        this.router.navigate(['../navigation-extras-destination'], { state: this.model });
+        this.router.navigate(['../navigation-extras-destination'], { relativeTo: this.activatedRoute, state: this.model });
     }
 
 }
