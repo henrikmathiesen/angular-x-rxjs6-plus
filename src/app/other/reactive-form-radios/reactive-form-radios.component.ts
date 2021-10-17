1ce8fedcdd959936ba0518edfca9a94a2129ff68
henrikmathiesen <henrik_mathiesen@hotmail.com>
2021-10-17 08:21:43 +0200

    -testing navigating away, and binding

--- src/app/other/reactive-form-radios/reactive-form-radios.component.ts
+++ src/app/other/reactive-form-radios/reactive-form-radios.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
     templateUrl: './reactive-form-radios.component.html'
 })
 export class ReactiveFormRadios {
-    
+
+    form = new FormGroup({
+        food: new FormControl(null),
+        name: new FormControl(null)
+    });
+
 }
