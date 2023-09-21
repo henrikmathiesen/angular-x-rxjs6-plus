e2c4e4e2e034534fbc183ca5c179a45448e7619b
Henrik Mathiesen <henrik_mathiesen@hotmail.com>
2023-09-21 12:50:37 +0200

    Update global-error-handler.ts

--- src/app/services/global-error-handler.ts
+++ src/app/services/global-error-handler.ts
@@ -1,7 +1,29 @@
 import { ErrorHandler, Injectable } from '@angular/core';
 
+/* 
+import { ErrorHandler, Injectable, Injector, NgZone } from '@angular/core';
+import { Router } from '@angular/router';
+*/
+
 @Injectable()
 export class GlobalErrorHandler implements ErrorHandler {
+
+  /* 
+    constructor(
+        private injector: Injector
+    ) { }
+
+    handleError(error: Error) {
+        const router = this.injector.get(Router);
+        const ngZone = this.injector.get(NgZone);
+
+        // and logging perhaps
+
+        ngZone.run(() => {
+            router.navigate(['/my-error-view']);
+        });
+    }
+  */
 
   handleError() {
     console.log('GLOBAL ERROR HANDLER');
